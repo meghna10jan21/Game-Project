49ead7f930a28c064abbf951b06271633018bf38
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-06 17:36:37 -0500

    changes

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ window.onload= ()=>{
 
 const cardsList = [
     { name: 'collectionfish', img: 'assets/collectionfish' },
+    { name: 'collectionfish', img: 'assets/collectionfish' },
+    { name: 'coralreff', img: 'assets/coralreff.jpeg' },
     { name: 'coralreff', img: 'assets/coralreff.jpeg' },
     { name: 'Dolphines', img: 'assets/Dolphines.jpeg'},
+    { name: 'Dolphines', img: 'assets/Dolphines.jpeg'},
+    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg' },
     { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg' },
     { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg'},
+    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg'},
+    { name: 'Turtle', img: 'assets/Turtle.jpeg'},
     { name: 'Turtle', img: 'assets/Turtle.jpeg'},
 ]
-
+    let result = 0;
+    let cardsclick = 0;
+    let 
    const cards= document.querySelectorAll(".innercards")
 for (let i=0; i<cardsList.length; i++){
     let card = cards[i]
@@ -16,15 +24,19 @@ for (let i=0; i<cardsList.length; i++){
     frontcards.src = "./assets/underwater.jpeg"
     card.addEventListener('click', flip)
     card.append(frontcards)
-
 }
     
 }
 
-function flip(){
-    const backcard = document.querySelectoAll(i)
-    cardsSelect.push(cardsList[cardsList[backcard].name])
+function gamestart(){
+    const selection = document.querySelectoAll(i)
+    cardsSelect.push(cardsList[selection].name)
 
+}
+
+
+function Shufflecards(){
+   cardsList.sort(() => random()) 
 }
 
 
