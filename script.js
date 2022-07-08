9225d0b978368f1f935d25b5352f6d120187c00f
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-08 14:36:34 -0500

    changes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 window.onload= ()=>{
 
 const cardsList = [
-    { name: 'collectionfish', img: 'assets/collectionfish' },
-    { name: 'collectionfish', img: 'assets/collectionfish' },
-    { name: 'coralreff', img: 'assets/coralreff.jpeg' },
-    { name: 'coralreff', img: 'assets/coralreff.jpeg' },
-    { name: 'Dolphines', img: 'assets/Dolphines.jpeg'},
-    { name: 'Dolphines', img: 'assets/Dolphines.jpeg'},
-    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg' },
-    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg' },
-    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg'},
-    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg'},
-    { name: 'Turtle', img: 'assets/Turtle.jpeg'},
-    { name: 'Turtle', img: 'assets/Turtle.jpeg'},
-]
+    { name: 'collectionfish', img: 'assets/collectionfish.jpeg', },
+    { name: 'collectionfish', img: 'assets/collectionfish.jpeg', },
+    { name: 'coralreff', img: 'assets/coralreff.jpeg', },
+    { name: 'coralreff', img: 'assets/coralreff.jpeg', },
+    { name: 'Dolphines', img: 'assets/Dolphines.jpeg',},
+    { name: 'Dolphines', img: 'assets/Dolphines.jpeg',},
+    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg', },
+    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg', },
+    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg',},
+    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg',},
+    { name: 'Turtle', img: 'assets/Turtle.jpeg',},
+    { name: 'Turtle', img: 'assets/Turtle.jpeg',},
+];
     let result = 0;
     let cardsclick = 0;
-    let Firstcardselection = [];
-    let secondcardselection = [];
+    let firstcard= [];
+    let secondcard = [];
     
    const cards= document.querySelectorAll(".innercards")
 for (let i=0; i<cardsList.length; i++){
@@ -30,11 +30,29 @@ for (let i=0; i<cardsList.length; i++){
     
 }
 
-function gamestart(){
-    const selection = document.querySelectoAll(i)
+function flip(){
+    const selection = this.information.id;
+    let firstcard = document.getElementsByClassName(".value")
     cardsSelect.push(cardsList[selection].name)
 
 }
+
+function Matchcards(){
+    const cards = document.querySelectorAll('img')
+    if(firstcard[0] === secondcard[0]){
+        alert('matched');
+        removeEventListener.firstcard && secondcard;
+    }
+       else (firstcard[0] !== secondcard[0]);{
+        alert('NOT A MATCH');
+        cards(flipback); 
+       }
+    
+       }
+       
+    
+    
+
 
 
 function Shufflecards(){
