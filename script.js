d926d34bb686329354e2c46a12072648b5df9fa1
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-09 07:31:49 -0500

    changes

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
-window.onload= ()=>{
-
-const cardsList = [
-    { name: 'collectionfish', img: 'assets/collectionfish.jpeg', },
-    { name: 'collectionfish', img: 'assets/collectionfish.jpeg', },
-    { name: 'coralreff', img: 'assets/coralreff.jpeg', },
-    { name: 'coralreff', img: 'assets/coralreff.jpeg', },
-    { name: 'Dolphines', img: 'assets/Dolphines.jpeg',},
-    { name: 'Dolphines', img: 'assets/Dolphines.jpeg',},
-    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg', },
-    { name: 'Jellyfish', img: 'assets/Jellyfish.jpeg', },
-    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg',},
-    { name: 'Tropicalfish', img: 'assets/Tropicalfish.jpeg',},
-    { name: 'Turtle', img: 'assets/Turtle.jpeg',},
-    { name: 'Turtle', img: 'assets/Turtle.jpeg',},
-];
-    let result = 0;
-    let cardsclick = 0;
-    let firstcard= [];
-    let secondcard = [];
-    
-   const cards= document.querySelectorAll(".innercards")
+let cardsList = '';
+window.onload= ()=> { 
+    const cards= document.querySelectorAll(".cards")
 for (let i=0; i<cardsList.length; i++){
     let card = cards[i]
-    const frontcards = document.createElement('img');
-    frontcards.src = "./assets/underwater.jpeg"
-    card.addEventListener('click', flip)
+    card.addEventListener('click', flip);
     card.append(frontcards)
 }
+
+}    
+
+function flip(){
     
 }
 
-function flip(){
-    const selection = this.information.id;
-    let firstcard = document.getElementsByClassName(".value")
-    cardsSelect.push(cardsList[selection].name)
-
-}
+let firstcard='';
+let secondcard='';
 
 function Matchcards(){
     const cards = document.querySelectorAll('img')
+    let img = setAttribute('src', './assets/underwater.jpeg');
     if(firstcard[0] === secondcard[0]){
         alert('matched');
         removeEventListener.firstcard && secondcard;
@@ -47,13 +27,8 @@ function Matchcards(){
         alert('NOT A MATCH');
         cards(flipback); 
        }
+    }
     
-       }
-       
-    
-    
-
-
 
 function Shufflecards(){
    cardsList.sort(() => random()) 
