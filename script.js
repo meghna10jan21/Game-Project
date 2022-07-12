a1a7d6cef03a101884ed6094aeab3be1fc050301
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-12 12:14:00 -0500

    NEW change

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-let cardsList = '';
-window.onload= ()=> { 
-    const cards= document.querySelectorAll(".cards")
-for (let i=0; i<cardsList.length; i++){
-    let card = cards[i]
-    card.addEventListener('click', flip);
-    card.append(frontcards)
-}
 
-}  
+    const cards=document.querySelectorAll("innerCard");
+    cards.forEach(card=> card.addEventListener('click', flip));
 
- let firstcard;
- let secondcard;
-
-function Matchcards(){
-    const cards = document.querySelectorAll('img')
-    let img = setAttribute('src', './assets/underwater.jpeg');
-    if(firstcard[0] === secondcard[0]){
-        alert('matched');
-       firstcard.removeEventListener("click", flip);
-       secondcard.removeEventListener("click", flip);
-    }
-       else (firstcard[0] !== secondcard[0]);{
-        alert('NOT A MATCH');
-        cards(flipback); 
-       }
-    }
-
-function flip(){
-        const selectedcard = document.getElementsByClassName('value')
-        cardsClicked.push([i])   
+    let firstCard;
+    let secondCard;    
+    function matchCards(){
+        const cards = document.querySelectorAll('img')
+       // let img = setAttribute('src', './assets/underwater.jpeg');
+        if(firstCard[0] === secondCard[0]){
+            alert('matched');
+           firstCard.removeEventListener("click", flip);
+           secondCard.removeEventListener("click", flip);
         }
+           else if(firstCard[0] !== secondCard[0]);{
+            alert('NOT A MATCH');
+            cards(flipback); 
+           }
+  
 
-function Endgame(){
-    const endgame = document.addEventListener("click", score);
-    alert('YOU WON');
-    return(Shufflecards);
-
+           
 }
-
-function playagain(){
-    const button = document.createElement("button");
-    button.innerhtml = "Playagain";
-    document.body.appendChild(button); 
-}
-function Shufflecards(){
-    cardsList.sort(() => random()) 
- }
