99fc95884b0c7006ade9ae5987669578610618c3
Meghna Patel <meghna10jan21@yahoo.com>
2022-07-12 17:24:34 -0500

    last changes

--- script.js
+++ script.js
@@ -2,24 +2,42 @@
     const cards=document.querySelectorAll("innerCard");
 
     function flip(){
-        this.classlist.cards.add('flip'); 
+        this.classlist.cards.add('flip');
+        //let img = setAttribute('src', './assets/underwater.jpeg');
     }
     cards.forEach(card=> card.addEventListener('click', flip));
 
-    let cardOne;
-    let cardTwo;    
-    function matchCards(){
+    let cardOne='';
+    let cardTwo=''; 
+
+    function gameStart(){
         const cards = document.querySelectorAll('#value')
-       // let img = setAttribute('src', './assets/underwater.jpeg');
         if(cardOne[0] === cardTwo[0]){
             alert('matched');
-           firstCard.removeEventListener("click", flip);
-           secondCard.removeEventListener("click", flip);
+           cardOne.removeEventListener("click", flip);
+           cardTwo.removeEventListener("click", flip);
         }
            else if(cardOne[0] !== cardTwo[0]);{
             alert('NOT A MATCH');
             cards(flipback); 
            }
+           return(value++)
+}
+    function endGame(){
+    const endGame = document.getElementsByClassName("click", value);
+    if(cardOne && cardTwo === 'null'); 
+    alert('YOU WON');
+    return(shuffleCards);
+}
+
+    function shuffleCards(){
+    cardList.sort(()=> random());
+}
+
+    function playAgain(){
+    const button = document.createElement("button");
+    button.innerhtml = "playAgain";
+    document.body.appendChild(button);
 }
 
 
